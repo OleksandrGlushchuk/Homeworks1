90c8b5ca1e6e380a92f59dce660c3b304b5857ad
OlexandrGlushchuk <glushchuk259@gmail.com>
2022-05-23 09:07:13 +0300

    initial commit homework-2 01

--- homework-1/engine/window/window.cpp
+++
@@ -1,9 +0,0 @@
-#include "window.h"
-
-void Window::OnResize()
-{
-	GetClientRect(hwnd, &screen);
-	image.resize(screen.right * screen.bottom);
-	bitmap_info.bmiHeader.biWidth = screen.right;
-	bitmap_info.bmiHeader.biHeight = screen.bottom;
-}
