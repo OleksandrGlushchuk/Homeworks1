163bd84f10878b830dcaa573236c100d3b842c52
OlexandrGlushchuk <glushchuk259@gmail.com>
2022-05-27 13:35:47 +0300

    rm -r homework-2

--- homework-2/engine/window/window.cpp
+++
@@ -1,9 +0,0 @@
-#include "window.h"
-
-void Window::OnResize()
-{
-	GetClientRect(hwnd, &screen);
-	image.resize((screen.right/2) * (screen.bottom/2));
-	bitmap_info.bmiHeader.biWidth = screen.right/2;
-	bitmap_info.bmiHeader.biHeight = screen.bottom/2;
-}
