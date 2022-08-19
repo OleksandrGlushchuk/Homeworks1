1cb2951339561b3a3498cc802f11a02dcbba6117
OlexandrGlushchuk <glushchuk259@gmail.com>
2022-08-19 15:26:32 +0300

    texture loader v2.1

--- RenderProject/engine/render/TextureManager.h
+++ RenderProject/engine/render/TextureManager.h
@@ -19,7 +19,7 @@ namespace engine
 		static void init();
 		static void deinit();
 		static TextureManager& instance();
-		const engine::DxResPtr<ID3D11ShaderResourceView>& LoadTexture(const std::wstring& fileName, bool isSRGB = true);
+		const engine::DxResPtr<ID3D11ShaderResourceView>& LoadTexture(const std::wstring& fileName, bool need_to_force_sRGB = false);
 
 		const engine::DxResPtr<ID3D11ShaderResourceView>& GetTexture(const std::wstring& fileName);
 	};
